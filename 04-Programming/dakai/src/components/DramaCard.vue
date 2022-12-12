04f94710047a077d5d032411dd25755d7c36248b
Nsstring <nsstringgamedesigner@gmail.com>
2022-12-12 15:50:33 +0800

    1212 onclass
    DramaCard.vue now loads every pics in assets.

---
+++ 04-Programming/dakai/src/components/DramaCard.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <img :src="img1" alt="">
+    </div>
+</template>
+
+<style scoped lang="scss">
+</style>
+
+<script>
+    // load every picture files in context. 
+    const context = require.context("../assets/", false, /\.(png|jpg|gif|jpeg|webp|ico)$/);
+    //let context turn into routes
+    const images = context.keys().map(context);
+
+    export default{
+        data(){
+            return{
+                images: images,
+                img1: images[1],
+            }
+        },
+        mounted() {
+        },
+        methods:{
+        }
+    }
+</script>
--- 04-Programming/dakai/src/views/AboutView.vue
+++ 04-Programming/dakai/src/views/AboutView.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <DramaCard/>
   </div>
 </template>
+
+<script>
+  import DramaCard from '../components/DramaCard.vue'
+
+  export default {
+    name: 'CardView',
+    components:{
+      DramaCard
+    }
+  }
+</script>
